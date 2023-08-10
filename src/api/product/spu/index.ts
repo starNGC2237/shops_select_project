f64950dcfa20fd67841cbf9420e5a535e432157a
zhilutianji <starmars_lu@163.com>
2023-08-10 16:12:06 +0800

    feat: 81

--- src/api/product/spu/index.ts
+++ src/api/product/spu/index.ts
@@ -1,7 +1,17 @@
 import request from "@/utils/request";
-import type { HasSpuResponseData } from "./types.ts";
+import type {
+  AllTrademarkResponseData,
+  HasSaleAttrResponseData,
+  HasSpuResponseData,
+  SaleAttrResponseData,
+  SpuHasImg,
+} from "./types.ts";
 enum API {
   HASSPU_URL = "/admin/product/",
+  ALLTRADEMARK_URL = "/admin/product/baseTrademark/getTrademarkList/",
+  IMAGE_URL = "/admin/product/spuImageList/",
+  SPUHASSALEATTRE_URL = "/admin/product/spuSaleAttrList/",
+  ALLSALEATTR_URL = "/admin/product/baseSaleAttrList",
 }
 export const reqHasSpu = (
   page: number,
@@ -11,3 +21,12 @@ export const reqHasSpu = (
   request.get<any, HasSpuResponseData>(
     API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`
   );
+
+export const reqAllTrademark = () =>
+  request.get<any, AllTrademarkResponseData>(API.ALLTRADEMARK_URL);
+export const reqSpuImageList = (spuId: number | string) =>
+  request.get<any, SpuHasImg>(API.IMAGE_URL + `${spuId}`);
+export const reqSpuHasSaleAttr = (spuId: number | string) =>
+  request.get<any, SaleAttrResponseData>(API.SPUHASSALEATTRE_URL + `${spuId}`);
+export const reqAllSaleAttr = () =>
+  request.get<any, HasSaleAttrResponseData>(API.ALLSALEATTR_URL);
--- src/api/product/spu/types.ts
+++ src/api/product/spu/types.ts
@@ -23,3 +23,54 @@ export interface HasSpuResponseData extends ResponseData {
     records: Records;
   };
 }
+export interface Trademark {
+  id: number;
+  tmName: string;
+  logoUrl: string;
+}
+
+export interface AllTrademarkResponseData extends ResponseData {
+  data: Trademark[];
+}
+export interface SpuImg {
+  id: number;
+  createTime: string;
+  updateTime: string;
+  spuId: number;
+  imgName: string;
+  imgUrl: string;
+}
+
+export interface SpuHasImg extends ResponseData {
+  data: SpuImg[];
+}
+export interface SaleAttrValue {
+  id?: number;
+  createTime: string;
+  updateTime: string;
+  spuId: number;
+  baseSaleAttrId: number;
+  saleAttrValueName: string;
+  saleAttrName?: string;
+  isChecked?: boolean;
+}
+export type spuSaleAttrValueList = SaleAttrValue[];
+export interface SaleAttr {
+  id?: number;
+  createTime: string;
+  updateTime: string;
+  spuId: number;
+  baseSaleAttrId: number;
+  saleAttrName?: string;
+  spuSaleAttrValueList: spuSaleAttrValueList;
+}
+export interface SaleAttrResponseData extends ResponseData {
+  data: SaleAttr[];
+}
+export interface HasSaleAttr {
+  id: number;
+  name: string;
+}
+export interface HasSaleAttrResponseData extends ResponseData {
+  data: HasSaleAttr[];
+}
