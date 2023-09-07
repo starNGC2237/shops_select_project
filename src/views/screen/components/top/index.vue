78f1169143ba51a31a23cfedb799f14d0c766150
zhilutianji <starmars_lu@163.com>
2023-09-07 17:25:13 +0800

    feat: 131

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn" @click="goHome">首页</span>
+    </div>
+    <div class="center">
+      <div class="title">智慧旅游可视化大数据平台</div>
+    </div>
+    <div class="right">
+      <span class="rbtn">统计报告</span>
+      <span class="time">当前时间：{{ time }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import dayjs from "dayjs";
+
+let $router = useRouter();
+let time = ref(dayjs().format("YYYY-MM-DD HH:mm:ss"));
+let timer = ref(null);
+
+const goHome = () => {
+  $router.push("/home");
+};
+
+onMounted(() => {
+  timer.value = setInterval(() => {
+    time.value = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  }, 1000);
+});
+onBeforeUnmount(() => {
+  clearInterval(timer.value);
+});
+</script>
+
+<style scoped lang="scss">
+.top {
+  width: 100px;
+  height: 40px;
+  display: flex;
+  .right {
+    flex: 1.5;
+    background: url(../../images/dataScreen-header-left-bg.png) no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .rbtn {
+      width: 150px;
+      height: 40px;
+      float: left;
+      background: url(../../images/dataScreen-header-btn-bg-r.png) no-repeat;
+      background-size: 100% 100%;
+      text-align: center;
+      line-height: 40px;
+      color: #29fcff;
+      font-size: 20px;
+    }
+    .time {
+      color: #29fcff;
+      text-align: center;
+      margin-right: 10px;
+      font-size: 20px;
+    }
+  }
+  .left {
+    flex: 1.5;
+    background: url(../../images/dataScreen-header-left-bg.png) no-repeat;
+    background-size: cover;
+    .lbtn {
+      width: 150px;
+      height: 40px;
+      float: right;
+      background: url(../../images/dataScreen-header-btn-bg-l.png) no-repeat;
+      background-size: cover;
+      text-align: center;
+      line-height: 40px;
+      color: #29fcff;
+      font-size: 20px;
+      cursor: pointer;
+    }
+  }
+  .center {
+    flex: 2;
+    .title {
+      width: 100%;
+      height: 74px;
+      background: url(../../images/dataScreen-header-center-bg.png) no-repeat;
+      background-size: 100% 100%;
+      text-align: center;
+      line-height: 74px;
+      color: #29fcff;
+      font-size: 30px;
+    }
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,76 @@
 <template>
-  <div>大屏</div>
+  <div class="container">
+    <div class="screen" ref="screenRef">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">左</div>
+        <div class="center">中</div>
+        <div class="right">右</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import Top from "./components/top/index.vue";
 
-<style scoped></style>
+let screenRef = ref<HTMLElement>();
+
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w;
+  const wh = window.innerHeight / h;
+  return Math.min(ww, wh);
+};
+
+onMounted(() => {
+  screenRef.value &&
+    (screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`);
+  window.addEventListener("resize", () => {
+    screenRef.value &&
+      (screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`);
+  });
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", () => {
+    screenRef.value &&
+      (screenRef.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`);
+  });
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    width: 1920px;
+    height: 1080px;
+    transform-origin: left top;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1;
+      }
+      .left {
+        flex: 1;
+      }
+      .center {
+        flex: 2;
+      }
+    }
+  }
+}
+</style>
