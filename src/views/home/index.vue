95a714acf02a85bdf8ba3754667c81c057cf5764
zhilutianji <starmars_lu@163.com>
2023-11-02 21:11:30 +0800

    feat: 优化首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -19,7 +19,7 @@
         <div class="things">
           <el-statistic :value="38">
             <template #title>
-              <div style="display: flex; justify-content: flex-end">代办</div>
+              <div style="display: flex; justify-content: flex-end">待办</div>
             </template>
             <template #suffix>/100</template>
           </el-statistic>
@@ -46,6 +46,7 @@
                 <el-link type="primary" :underline="false">更多</el-link>
               </div>
             </template>
+            暂无
           </el-card>
           <el-card
             body-style="padding:30px"
@@ -58,15 +59,45 @@
                 <el-link type="primary" :underline="false">更多</el-link>
               </div>
             </template>
+            暂无
           </el-card>
         </div>
         <div class="right">
-          <el-card body-style="padding:30px" shadow="never">
+          <el-card
+            body-style="padding:0px;border:0;border-left: 1px solid var(--el-card-border-color);"
+            shadow="never"
+            class="paths"
+          >
             <template #header>
               <div class="card-header">
                 <h2>快捷导航</h2>
               </div>
             </template>
+            <div style="display: flex; flex-wrap: wrap">
+              <el-card
+                @click="$router.push(item.path)"
+                shadow="hover"
+                body-style="padding:0px;"
+                v-for="item in pathsData"
+                :key="item.path"
+                class="paths_item"
+              >
+                <div
+                  style="
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                  "
+                >
+                  <el-icon style="width: 20px; height: 20px">
+                    <component :is="item.icon"></component>
+                  </el-icon>
+                  <span style="margin-top: 1rem">
+                    {{ item.name }}
+                  </span>
+                </div>
+              </el-card>
+            </div>
           </el-card>
         </div>
       </div>
@@ -77,8 +108,31 @@
 <script setup lang="ts">
 import useUserStore from "@/store/modules/user";
 import { getMoment } from "@/utils/time";
+import { ref } from "vue";
 
 let userStore = useUserStore();
+let pathsData = ref([
+  {
+    name: "首页",
+    icon: "HomeFilled",
+    path: "/home",
+  },
+  {
+    name: "数据大屏",
+    icon: "Platform",
+    path: "/screen",
+  },
+  {
+    name: "商品管理",
+    icon: "Goods",
+    path: "/product",
+  },
+  {
+    name: "权限管理",
+    icon: "Lock",
+    path: "/acl",
+  },
+]);
 </script>
 
 <style scoped lang="scss">
@@ -122,12 +176,12 @@ let userStore = useUserStore();
     width: 100%;
     justify-content: space-between;
     & > .left {
-      width: calc(60% - 20px);
+      width: calc(70% - 7.5px);
       display: flex;
       flex-direction: column;
     }
     & > .right {
-      width: calc(40% - 20px);
+      width: calc(30% - 7.5px);
       display: flex;
       flex-direction: column;
     }
@@ -140,6 +194,7 @@ let userStore = useUserStore();
   height: 30px;
   & > h2 {
     margin: 0;
+    font-weight: 700;
   }
 }
 .things {
@@ -147,6 +202,30 @@ let userStore = useUserStore();
   align-items: center;
   & > .el-statistic {
     margin-right: 2rem;
+  }
+}
+.paths {
+  border: 0;
+  ::v-deep(.el-card__header) {
+    border: 1px solid var(--el-card-border-color);
+  }
+  .paths_item {
+    width: calc(100% / 3);
+    padding: 1rem;
+    box-sizing: border-box;
+    border-radius: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 0;
+    border-right: 1px solid var(--el-card-border-color);
+    border-bottom: 1px solid var(--el-card-border-color);
+    cursor: pointer;
+    &:hover {
+      z-index: 10;
+      box-shadow: 0 1px 2px -2px rgba(0, 0, 0, 0.16),
+        0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09);
+    }
   }
 }
 </style>
