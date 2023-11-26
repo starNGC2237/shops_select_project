70d0f2d615a1dc38f396ca33e5353ca976683a53
zhilutianji <starmars_lu@163.com>
2023-11-26 21:47:35 +0800

    feat: 登陆优化

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -23,6 +24,9 @@ export default defineConfig(({ mode }) => {
       }),
       Components({
         resolvers: [ElementPlusResolver()],
+      }),
+      ElementPlus({
+        // options
       }),
     ],
     resolve: {
